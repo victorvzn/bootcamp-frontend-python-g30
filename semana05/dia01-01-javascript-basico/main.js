bde5a5e0dbc1bc4b82cc9a517f3c34e2b842cafa
Victor Villazón <victorzn23@gmail.com>
2025-12-01 20:08:05 -0500

    semana05, dia01, 01, javascript básico

--- semana05/dia01-01-javascript-basico/main.js
+++ semana05/dia01-01-javascript-basico/main.js
@@ -1,1 +1,43 @@
-console.log('¡Hola Javascript desde un archivo externo!')
+// COMENTARIOS
+
+// console.log('¡Hola Javascript desde un archivo externo!')
+
+/*
+  Esto
+  es
+  un
+  comentario
+  de
+  múltiples
+  líneas
+*/
+
+// TIPOS DE DATOS
+
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+// https://lenguajejs.com/javascript/
+
+// PRIMITIVOS: 
+//   * Number (Números), String (cadena de texto), Boolean (true, false)
+//   * undefined, null, BigInt, Symbol, ...
+
+// NUMBERS, para js los enteros, decimales, son Numbers
+
+console.log(20)
+console.log(12.50)
+console.log(-56)
+
+console.log(typeof 20)     // number
+console.log(typeof 12.50)  // number
+console.log(typeof -56)    // number
+
+// STRING o Cadenas de texto
+
+console.log('Sebastian')
+console.log("Sebastian")
+console.log('Sebastian\'s')
+
+console.log(typeof 'Sebastian')     // string
+console.log(typeof "Sebastian")     // string
+console.log(typeof 'Sebastian\'s')  // string
+

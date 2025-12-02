e91463cfac11630efc1ca5984604ee6da8dc5596
Victor Villazón <victorzn23@gmail.com>
2025-12-01 20:58:26 -0500

    semana05, dia01, 01, javascript básico

--- semana05/dia01-01-javascript-basico/main.js
+++ semana05/dia01-01-javascript-basico/main.js
@@ -61,7 +61,7 @@ console.log(typeof nombre)  // undefined
 let apellido = null
 
 console.log(apellido)
-console.log(typeof apellido) // object❓
+console.log(typeof apellido) // object❓ No usemos el typeof para verificar si el null en su lugar usemos el === Ej. apellido === null
 
 // BigInt (Tipo de dato primitivo)
 // Symbol (Tipo de dato primitivo)
@@ -69,3 +69,37 @@ console.log(typeof apellido) // object❓
 
 // VARIABLES Y CONSTANTES
 
+// Nos sirven para guardar datos en memoria y reutilizarlos o modificarlos durante el tiempo que nuestro programa se esté ejecutando.
+
+// ECMASCRIPT 6 === ES6
+// const, para valores fijos
+// let, se usa cuando el valor cambia
+
+// ECMASCRIPT 5 === ES5
+// var -> Forma de declarar una variable pero no es recomendable su uso actualmente ❌
+
+// CONST -> ES6 (Si es recomendable usar actualmente)
+
+const edad = 55 // Es obligatorio inicializarlo
+const _edad = 25
+const edadDeMiHijo = 17  // Estamos usando el estilo camelCase. Hay otros PascalCase, snake_case, kebab-case
+const PI = 3.14
+const nombre2 = "Victor"
+
+// edad = 33 // ❌ Uncaught TypeError: Assignment to constant variable.
+// edad-de-mi-hijo = 33 // ❌ Uncaught SyntaxError: Invalid left-hand side in assignment
+
+console.log(edad)
+console.log(edad)
+
+// LET -> ES5 (Si es recomendable usar actualmente)
+
+let edad2 = 35 // Nota: no es obligatorio inicializarlo
+
+edad2 = 89    // Lo sobreescribe con el mismo tipo, number
+edad2 = "36" // Lo sobreescribe con un tipo de dato nuevo, string
+edad2 = true
+edad2 = 87
+
+console.log(edad2)
+console.log(typeof edad2)

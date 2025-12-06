addb106a7c25fe56d2d53f02577633673d545b0c
Victor Villazón <victorzn23@gmail.com>
2025-12-05 19:35:34 -0500

    semana05, dia02, 01, javascript intermedio

--- semana05/dia02-01-javascript-intermedio/main.js
+++ semana05/dia02-01-javascript-intermedio/main.js
@@ -1,1 +1,21 @@
-console.log('Hola JS')
+// FUNCIONES (Son bloques de código que podemos reutilizar)
+
+// Declaración
+function nombreDeLaFuncion() {
+  // Cuerpo de la función y aquí va la lógica a ejecutar y las que vamos a reutlizar
+  console.log('Hola soy una función')
+  console.log('Soy un bloque de código')
+}
+
+// Ejecutar, estamos llamando a la función
+nombreDeLaFuncion()
+nombreDeLaFuncion()
+
+// Funciones sin parámetros
+
+function imprimirMiNombre() {
+  console.log('¡Hola soy Victor!')
+  console.log('Y soy fullstack')
+}
+
+imprimirMiNombre()

8203f53731f5a2e27d77f5183aae5bb6227ff479
Victor Villazón <victorzn23@gmail.com>
2025-12-10 21:43:19 -0500

    semana05, dia02, 01, javascript intermedio

--- semana05/dia02-01-javascript-intermedio/main.js
+++ semana05/dia02-01-javascript-intermedio/main.js
@@ -451,6 +451,7 @@ const miArreglo = []
 
 languages.forEach(
   function(language, index) {
+    console.log(index, language)
     if (index > 3) {
       miArreglo.push('hola--' + language)
     }
@@ -458,3 +459,24 @@ languages.forEach(
 )
 
 console.log(miArreglo)
+
+// TODO: Investigar los métodos indexOf, find, every, some, findIndex, flat
+
+// EJERCICIO: Eliminar duplicados 
+// Usando filter() + indexOf() nos quedamos solo con la primera aparición.
+const nombres = ["Ana", "Luis", "Ana", "Pedro", "Luis"];
+
+// Resultado esperado: ["Ana", "Luis", "Pedro"]
+
+// EJERCICIO: Validar si todos los correos contienen “@” y quedarte solo con los válidos
+
+const correos = ["test@gmail.com", "infohotmail.com", "admin@outlook.com"];
+
+// Resultado esperado: ["test@gmail.com", "admin@outlook.com"]
+
+// EJERCICIO: Encontrar el primer número mayor a 100 y verificar si hay varios
+// Combina: find + some + filter
+
+const nums2 = [10, 55, 120, 80, 200];
+
+

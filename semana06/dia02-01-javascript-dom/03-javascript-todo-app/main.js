c610b80dd17c4ad2eeaf7d047dfa4a6a02be519b
Victor Villazón <victorzn23@gmail.com>
2025-12-15 20:52:15 -0500

    semana06, dia01, 02, javascript dom

---
+++ semana06/dia02-01-javascript-dom/03-javascript-todo-app/main.js
@@ -0,0 +1,5 @@
+const taskInput = document.getElementById('taskInput')
+const taskAdd = document.querySelector('.task__add')
+const taskList = document.querySelector('.task__list')
+
+console.log(taskList)

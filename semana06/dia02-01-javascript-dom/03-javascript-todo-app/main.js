b99f9f357d0a1aee5f59f783a313e01f6f79bf52
Victor Villazón <victorzn23@gmail.com>
2025-12-15 21:00:54 -0500

    semana06, dia01, 02, javascript dom

--- semana06/dia02-01-javascript-dom/03-javascript-todo-app/main.js
+++ semana06/dia02-01-javascript-dom/03-javascript-todo-app/main.js
@@ -3,3 +3,14 @@ const taskAdd = document.querySelector('.task__add')
 const taskList = document.querySelector('.task__list')
 
 console.log(taskList)
+
+// EVENTOS
+
+// ELEMENTO.addEventListener(NOMBRE_EVENTO, CALLBACK)
+
+// CALLBACK: Es una función que usamos como parámetro de otras funciones.
+
+taskAdd.addEventListener('click', function(event) {
+  // Se ejecutará cuando hagamos click en el botón 'Añadir tarea'
+  console.log('Click!', taskInput.value)
+})

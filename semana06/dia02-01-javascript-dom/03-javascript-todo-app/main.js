a5c768a1f39956d7aa5a25ff5d377a3890185ab0
Victor Villazón <victorzn23@gmail.com>
2025-12-15 21:38:54 -0500

    semana06, dia01, 02, javascript dom

--- semana06/dia02-01-javascript-dom/03-javascript-todo-app/main.js
+++ semana06/dia02-01-javascript-dom/03-javascript-todo-app/main.js
@@ -13,4 +13,12 @@ console.log(taskList)
 taskAdd.addEventListener('click', function(event) {
   // Se ejecutará cuando hagamos click en el botón 'Añadir tarea'
   console.log('Click!', taskInput.value)
+
+  // MANEJO DEL DOM: Forma de crear elementos dinámicamente con el DOM de Javascript
+  // const button = document.createElement('button') // <button></button>
+  // button.textContent = 'Hola soy un botón'
+  // console.log(button)
+  // document.body.appendChild(button)
+
+  // TODO: 01 Añadir el elemento li al elemento con la clase task__list
 })

dc6aaf3ca21f7df693f6c657d9f2624ec8f74e15
Victor Villazón <victorzn23@gmail.com>
2025-12-17 22:17:13 -0500

    semana06, dia03, javascript todo app mejorado con tw

--- semana06/dia03-01-javascript-todo-app-tailwind-mejorado/main.js
+++ semana06/dia03-01-javascript-todo-app-tailwind-mejorado/main.js
@@ -2,6 +2,25 @@ const taskInput = document.querySelector('.task__input')
 const taskClear = document.querySelector('.task__clear')
 const taskList = document.querySelector('.task__list')
 
+let tasks = [
+  {
+    id: 'tarea-1',
+    title: 'Estudiar javascript',
+    completed: false
+  },
+  {
+    id: 'tarea-2',
+    title: 'Salir al receso',
+    completed: true
+  },
+  {
+    id: 'tarea-3',
+    title: 'Resolver el reto de la semana',
+    completed: false
+  }
+]
+
+
 // TODO - 01 Al presionar enter en la caja de texto debemos agregr una nueva tarea a la lista
 
 taskInput.addEventListener('keydown', function(event) {

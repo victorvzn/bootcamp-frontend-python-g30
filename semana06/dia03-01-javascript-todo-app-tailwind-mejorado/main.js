a891b145031c9e7142fd1b7fa3f7a4abe6eb9962
Victor Villazón <victorzn23@gmail.com>
2025-12-19 20:29:18 -0500

    semana06, dia03, javascript todo app mejorado con tw

--- semana06/dia03-01-javascript-todo-app-tailwind-mejorado/main.js
+++ semana06/dia03-01-javascript-todo-app-tailwind-mejorado/main.js
@@ -40,15 +40,15 @@ function renderTasks(tasks = []) {
         </div>
         <div class="flex gap-2">
           <button
-            class="border border-green-700 font-medium text-sm text-green-700 px-2 py-1 rounded-lg hover:bg-green-700 hover:text-white duration-300 cursor-pointer"
+            class="border border-green-700 font-medium text-sm text-green-700 px-1 py-1 rounded-lg hover:bg-green-700 hover:text-white duration-300 cursor-pointer w-8 h-8"
           >
-            Editar
+            <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-edit"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg>
           </button>
           <button
-            class="border border-red-700 font-medium text-sm text-red-700 px-2 py-1 rounded-lg hover:bg-red-700 hover:text-white duration-300 cursor-pointer"
+            class="border border-red-700 font-medium text-sm text-red-700 px-1 py-1 rounded-lg hover:bg-red-700 hover:text-white duration-300 cursor-pointer w-8 h-8 "
             data-id="${task.id}"
           >
-            Remover
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
           </button>
         </div>
       </li>

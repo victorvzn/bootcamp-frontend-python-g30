1f81e71729c492c59c214d4539c90baa81b9b668
Victor Villazón <victorzn23@gmail.com>
2026-01-05 21:56:21 -0500

    semana07, dia02, javascript trivia - resolviendo

--- semana07/dia01-03-js-trivia-reto/main.js
+++ semana07/dia01-03-js-trivia-reto/main.js
@@ -85,8 +85,44 @@ function respondQuestion(event, questionSelected) {
   })
 }
 
+function startAgain(event) {
+  correctAnswersCounter = 0
+  currentQuestionIndex = 0
+
+  renderQuestions()
+}
+
 function showResults(event) {
   // TODO: Terminar la pantalla de mostrar resultados con los datos respectivos para que sean dinámicos.
+
+  const title = correctAnswersCounter > 3 ? 'GANASTE' : 'MEJOR SUERTE LA PRÓXIMA'
+
+  questionAndResults.innerHTML = `
+    <section class="flex flex-col px-4 py-6 text-center bg-green-600 border rounded-lg shadow">
+      <p class="text-4xl font-medium text-gray-900 mb-4">
+        ¡${title}!
+      </p>
+      
+      <p class="text-md font-medium text-gray-900 mb-4">
+        Respondiste ${correctAnswersCounter} de ${questions.length}
+      </p>
+
+      <p class="text-md font-medium text-gray-900 mb-4">
+        Y este es tu puntaje: ${correctAnswersCounter * 150}
+      </p>
+
+      <img src="https://placehold.co/300x100" />
+
+      <div class="flex justify-end mt-10">
+        <button
+          class="text-white border border-blue-300 bg-blue-600 font-medium rounded-lg text-sm px-5 py-2.5 text-left mr-2 mb-2"
+          onclick="startAgain(event)"
+        >
+          Empezar de nuevo
+        </button>
+      </div>
+    </section>
+  `
 }
 
 function renderQuestions() {

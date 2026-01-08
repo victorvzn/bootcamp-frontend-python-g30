07caa126c62638169632bd6db05e67cafe77646e
Victor Villazón <victorzn23@gmail.com>
2026-01-07 22:19:46 -0500

    semana08, dia01, javascript http peticiones fetch

--- semana08/dia01-02-js-fetch-async-await/main.js
+++ semana08/dia01-02-js-fetch-async-await/main.js
@@ -1,2 +1,34 @@
-const url = "https://jsonplaceholder.typicode.com/posts"
+const url = "https://jsonplaceholder.typicode.com/users"
+
+// fetch(url)
+//   .then(respuesta => respuesta.json())
+//   .then(data => {
+//     console.log(data)
+//     renderTodos(data)
+//   })
+//   .catch(error => {
+//     console.log(error)
+//   })
+
+const fetchUsersSinRetorno = async () => {
+  const response = await fetch(url)
+
+  const data = await response.json()
+
+  console.log(data)
+}
+
+// fetchUsersSinRetorno()
+
+// console.log(fetchUsersSinRetorno()) // Promise {<pending>}
+
+const fetchUsersConnRetorno = async () => {  // Retorna una promesa (Promise)
+  const response = await fetch(url)
+
+  return await response.json()
+}
+
+fetchUsersConnRetorno() // Retorna una promesa
+  .then(users => console.log(users))
+
 

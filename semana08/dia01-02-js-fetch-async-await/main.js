24281679ea875ca159b0eb8b1afb6d8ad3dbe2b5
Victor Villazón <victorzn23@gmail.com>
2026-01-07 22:44:19 -0500

    semana08, dia01, javascript http peticiones fetch

--- semana08/dia01-02-js-fetch-async-await/main.js
+++ semana08/dia01-02-js-fetch-async-await/main.js
@@ -28,7 +28,44 @@ const fetchUsersConnRetorno = async () => {  // Retorna una promesa (Promise)
   return await response.json()
 }
 
-fetchUsersConnRetorno() // Retorna una promesa
-  .then(users => console.log(users))
+// fetchUsersConnRetorno() // Retorna una promesa
+//   .then(users => console.log(users))
 
 
+const fetchUsersConManejoDeErrores = async () => {
+  try {
+    const response = await fetch(url)
+
+    console.log(response.status)
+
+    if (response.status !== 200) {
+      console.log('Tuvimos problemas para cargar el recurso users')
+      // return
+      throw new Error('ERROR HTTP:' + response.status)
+    }
+
+    return await response.json() // STATUS CODE -> 200 OK
+  } catch(error) {
+    console.log(error)
+  }
+}
+
+const renderUsers = async (users = []) => {
+  const divApp = document.querySelector('#app')
+
+  let userList = ''
+
+  users.forEach(user => {
+    userList += `
+      <div>
+        <h2>${user.id} - ${user.name} (${user.email})</h2>
+        <p>${user.company.name}</p>
+      </div>
+    `
+  })
+
+  divApp.innerHTML = userList
+}
+
+fetchUsersConManejoDeErrores()
+  .then(users => renderUsers(users))

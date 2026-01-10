81f1ff903f13462d72a0c35291eb39b8fc51d089
Victor Villazón <victorzn23@gmail.com>
2026-01-09 21:06:04 -0500

    semana08, dia02, javascript fetch pokemons paginacion

--- semana08/dia02-01-js-fetch-pokemons-paginacion/main.js
+++ semana08/dia02-01-js-fetch-pokemons-paginacion/main.js
@@ -10,15 +10,36 @@ const fetchPokemons = async () => {
 
   const data = await response.json() // Convierte la respuesta en formato JS Object
 
-  // ????
+  // TODO: Agregar el id a cada pokemon dentro del arreglo results para usarlo en la imagen del pokemon
 
   return data
 }
 
-const renderPokemons = () => {
+const renderPokemons = (pokemons = []) => {
+  const pokemonsList = document.querySelector('#pokemonList')
+
+  let elements = ''
+
+  // TODO: Necesitamos agregar una imagen a cada pokemon
+
+  pokemons.forEach(pokemon => {
+    elements += `
+      <article>
+        <h2>${pokemon.name}</h2>
+        <img 
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg"
+          width="80"
+          height="80"
+        />
+      </article>
+    `
+  })
+
+  pokemonsList.innerHTML = elements
 }
 
 fetchPokemons()
   .then(data => {
-    console.log(data)
+    console.log(data.results)
+    renderPokemons(data.results)
   })

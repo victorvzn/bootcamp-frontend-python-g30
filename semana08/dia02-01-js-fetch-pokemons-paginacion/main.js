7a5d693f4fca7e56cf61b343b9429f5448600340
Victor Villazón <victorzn23@gmail.com>
2026-01-09 20:24:43 -0500

    semana08, dia02, javascript fetch pokemons paginacion

--- semana08/dia02-01-js-fetch-pokemons-paginacion/main.js
+++ semana08/dia02-01-js-fetch-pokemons-paginacion/main.js
@@ -1,2 +1,24 @@
 // TODO: Listar los pokemons en la consola usando la pokeapi
 // https://pokeapi.co/api/v2/pokemon
+
+// fetch -> Devuelve una promesa
+
+const fetchPokemons = async () => {
+  const API_URL = 'https://pokeapi.co/api/v2/pokemon'
+
+  const response = await fetch(API_URL) // Devuelve una promesa
+
+  const data = await response.json() // Convierte la respuesta en formato JS Object
+
+  // ????
+
+  return data
+}
+
+const renderPokemons = () => {
+}
+
+fetchPokemons()
+  .then(data => {
+    console.log(data)
+  })

af1c6ef528a6d2d91e718d227a9238ea30617adf
Victor Villazón <victorzn23@gmail.com>
2026-01-14 22:26:26 -0500

    semana08, dia02, javascript fetch pokemons paginacion

--- semana08/dia03-01-js-movie-app-crud-json-server-nodejs/public/main.js
+++ semana08/dia03-01-js-movie-app-crud-json-server-nodejs/public/main.js
@@ -1,4 +1,4 @@
-import { fetchMovies } from './servicios.js'
+import { createMovie, fetchMovies } from './servicios.js'
 
 const renderMovies = (movies = []) => {
   // TODO: Renderizar las peliculas usando la plantilla del tbody
@@ -43,6 +43,11 @@ const renderMovies = (movies = []) => {
   })
 
   movieList.innerHTML = html
+
+  // TODO: remover una película cuando presionamos el boton ❌
+
+  // TODO: Llenar el formulario de pelicula con la data de la pelicula seleccionado cuando presionamos en el botón ✏ y guardar la edición.
+
 }
 
 const moviesForm = document.querySelector('#moviesForm')
@@ -51,6 +56,54 @@ moviesForm.addEventListener('submit', async (event) => {
   event.preventDefault() // Evita que el formulario actualice la página
 
   console.log('Guardando la película...')
+
+  // 1. Extraer los datos del formulario
+
+  const peliculasForm = document.forms['moviesForm']
+
+  console.log({ peliculasForm })
+
+  const name = peliculasForm.name.value
+  const image = peliculasForm.image.value
+  const release = peliculasForm.release.value
+  const genreId = peliculasForm.genreId.value
+  const summary = peliculasForm.summary.value
+
+  console.log(name, image, release, genreId, summary)
+
+  // 2. Crear la película en el servidor
+
+  const newMovie = {
+    name,
+    image,
+    release,
+    genreId,
+    summary
+  }
+
+  try {
+    const response = await createMovie(newMovie)
+
+    if (!response.ok) {
+      console.log('La película no se guardó correctamente')
+      return
+    }
+
+    console.log('La película se guardó correctamente.')
+
+  } catch(error) {
+    console.log(error) // Errores inesperados
+  }
+
+  // 3. Actualizar la lista de películas
+
+  const movies = await fetchMovies()
+
+  renderMovies(movies)
+
+  // 4. Limpiamos el formulario
+
+  peliculasForm.reset()
 })
 
 fetchMovies()

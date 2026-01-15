c71590043c020c3e64af072dd92b1d3476c3bef8
Victor Villazón <victorzn23@gmail.com>
2026-01-14 20:56:47 -0500

    semana08, dia02, javascript fetch pokemons paginacion

--- semana08/dia03-01-js-movie-app-crud-json-server-nodejs/public/main.js
+++ semana08/dia03-01-js-movie-app-crud-json-server-nodejs/public/main.js
@@ -1,5 +1,9 @@
 import { fetchMovies } from './servicios.js'
 
+const renderMovies = (movies = []) => {
+  // TODO: Renderizar las peliculas usando la plantilla del tbody
+}
+
 fetchMovies()
   .then(data => {
     console.log(data)

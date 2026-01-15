77993e45270a2a637142334560d0c4e8876e2ced
Victor Villazón <victorzn23@gmail.com>
2026-01-14 21:45:25 -0500

    semana08, dia02, javascript fetch pokemons paginacion

--- semana08/dia03-01-js-movie-app-crud-json-server-nodejs/public/main.js
+++ semana08/dia03-01-js-movie-app-crud-json-server-nodejs/public/main.js
@@ -2,9 +2,50 @@ import { fetchMovies } from './servicios.js'
 
 const renderMovies = (movies = []) => {
   // TODO: Renderizar las peliculas usando la plantilla del tbody
+
+  const movieList = document.querySelector('.movies__list')
+
+  let html = ''
+
+  movies.forEach(movie => {
+    html += `
+      <tr>
+        <td>
+          ${movie.id}
+        </td>
+        <td>
+          <img
+            src="${movie.image}"
+            onerror="this.src='https://placehold.co/100x150?text=Error'"
+          />
+        </td>
+        <td>
+          <strong>${movie.name}</strong>
+          <div>
+            <strong>Release:</strong> ${movie.release}
+          </div>
+          <div>
+            <strong>Summary:</strong> ${movie.summary}
+          </div>
+        </td>
+        <td>
+          <div>
+            <button>
+              ✏
+            </button>
+            <button>
+              ❌
+            </button>
+          </div>
+        </td>
+      </tr>
+    `
+  })
+
+  movieList.innerHTML = html
 }
 
 fetchMovies()
   .then(data => {
-    console.log(data)
+    renderMovies(data)
   })

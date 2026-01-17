144460aa4d7d4dfb55c18d0c639a9b12f550c6e0
Victor Villazón <victorzn23@gmail.com>
2026-01-16 20:49:39 -0500

    semana09, dia01, react introducción

--- semana09/dia01-01-react-introduccion/src/App.jsx
+++ semana09/dia01-01-react-introduccion/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// 04 - Extensiones de VSCODE necesarias para usar React
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+// 03 - Componente usando múltiples líneas + Fragments (<></>)
+
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hola React!</h1>
+      <p>Estamos aprendiendo sobre componentes.</p>
     </>
   )
 }
 
 export default App
+
+// 02 - Componente base usando arrow functions
+
+// const Welcome = () => {
+
+//   return <h1>Bienvenidos a React.js con Arrow function!</h1>
+// }
+
+// export default Welcome
+
+// 01 - Componente base usando function
+
+// function Welcome() {
+//   return <h1>Bienvenidos a React.js!</h1>
+// }
+
+// export default Welcome
+
+// Un componente:
+// * Es una función, devuelve JSX, su nombre empieza con mayúscula.
+
+// Partes de una componente
+// 1. Imports (si los hay)
+// 2. Lógica (varibales, hooks, functiones)
+// 3. Return -> jsx
+// 4. Export
+
+// Reglas básicas de un componente
+// * Un solo elemento padre

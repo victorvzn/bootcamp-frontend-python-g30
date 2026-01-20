478d031d9fca5ddff6b0d41358c4630acbde980f
Victor Villazón <victorzn23@gmail.com>
2026-01-19 20:15:49 -0500

    semana09, dia02, react css eventos usestate hooks

--- semana09/dia02-01-react-eventos-usestate-hooks/src/App.jsx
+++ semana09/dia02-01-react-eventos-usestate-hooks/src/App.jsx
@@ -7,6 +7,8 @@ const App = () => {
     <div>
       <h1 className="textoBlanco">app</h1>
       <p style={{ backgroundColor: 'yellow', padding: '1rem' }}>Hola mundo</p>
+
+      <div className='text-3xl text-center p-8 bg-orange-500 text-white'>Tailwind CSS</div>
     </div>
   )
 }

97731e801483edc44b59c85cce13299b7364bbd4
Victor Villazón <victorzn23@gmail.com>
2026-01-19 23:06:53 -0500

    semana09, dia02, react css eventos usestate hooks

--- semana09/dia02-02-react-imc-app/src/App.jsx
+++ semana09/dia02-02-react-imc-app/src/App.jsx
@@ -1,31 +1,56 @@
-const App = () => {
-  
+import { useState } from "react"
 
+const App = () => {
+  // useState hook que nos ayuda a generar un estado para minipular datos dentro de nuestro componente
+  // useState, devuelve un arreglo con una variable de lectura y una función de escritura para cambiar el estado
+
+  const [peso, setPeso] = useState(80)
+  const [altura, setAltura] = useState(180)
+
+  // TODO: agregar a los input tipo range el evento onChange y cambiar el valor de cada campo(peso, altura)
+
+  const handlePeso = (event) => {
+    setPeso(event.target.value)
+  }
+
+  const handleAltura = (event) => {
+    setAltura(event.target.value)
+  }
+
+  const imc = peso / ((altura / 100) ** 2)
+
+  const imcDecimal = imc.toFixed(2)
+
+  // TODO: Obtener el valor IMC en base al siguiente rango:
+  // Bajo peso < 18.5, peso normal 18.5-24.9, sobrepeso 25-29.9, obesidad tipo 1 30-34.9, obesidad tipo 2 35-39.9, obesidad tipo 3 >=40, 
+ 
   return (
     <section className="w-[400px] bg-slate-200 p-4 mt-8 mx-auto rounded-md">
       <h1 className="text-3xl text-sky-600 text-center font-bold">IMC APP</h1>
 
       <div className="pt-4">
-        <p>Peso: 0kg</p>
+        <p>Peso: {peso}kg</p>
         <input
           type="range"
           min="50"
           max="200"
+          onChange={handlePeso}
         />
       </div>
 
       <div>
-        <p>Altura: 0cm</p>
+        <p>Altura: {altura}cm</p>
         <input
           type="range"
           min="50"
           max="200"
+          onChange={handleAltura}
         />
       </div>
 
-      <p className="font-bold">Tu IMC es 14.5</p>
+      <p className="font-bold">Tu IMC es {imcDecimal}</p>
 
-      <p className="font-bold text-2xl">Estado de IMC: Normal</p>
+      <p className="font-bold text-2xl">Estado de IMC: ????</p>
     </section>
   )
 }

aee52f1c60037b9d2889a71ccea3acd04356846f
Victor Villazón <victorzn23@gmail.com>
2026-01-21 19:31:53 -0500

    semana09, dia02, react css eventos usestate hooks

--- semana09/dia02-02-react-imc-app/src/App.jsx
+++ semana09/dia02-02-react-imc-app/src/App.jsx
@@ -24,6 +24,24 @@ const App = () => {
   // TODO: Obtener el valor IMC en base al siguiente rango:
   // Bajo peso < 18.5, peso normal 18.5-24.9, sobrepeso 25-29.9, obesidad tipo 1 30-34.9, obesidad tipo 2 35-39.9, obesidad tipo 3 >=40, 
  
+  const imcResult = () => {
+    let result = ''
+
+    if (imc < 18.5) {
+      result = 'Baja'
+    } else if (imc > 18.5 && imc <= 24.9) {
+      result = 'Normal'
+    } else if (imc > 25 && imc <= 29.9) {
+      result = 'Sobrepeso'
+    } else if (imc > 30) {
+      result = 'Obeso'
+    } else {
+      result = 'Datos incorrectos'
+    }
+
+    return result
+  }
+
   return (
     <section className="w-[400px] bg-slate-200 p-4 mt-8 mx-auto rounded-md">
       <h1 className="text-3xl text-sky-600 text-center font-bold">IMC APP</h1>
@@ -50,7 +68,7 @@ const App = () => {
 
       <p className="font-bold">Tu IMC es {imcDecimal}</p>
 
-      <p className="font-bold text-2xl">Estado de IMC: ????</p>
+      <p className="font-bold text-2xl">Estado de IMC: {imcResult()}</p>
     </section>
   )
 }

57d5ce745f795dd0374d90708c01f997115f6f3c
Victor Villazón <victorzn23@gmail.com>
2026-01-21 20:16:03 -0500

    semana09, dia02, react css eventos usestate hooks

--- semana09/dia03-01-react-calculadora/src/App.jsx
+++ semana09/dia03-01-react-calculadora/src/App.jsx
@@ -1,6 +1,43 @@
 const App = () => {
+  const operaciones = ['+', '-', '*']
+  const numeros = [
+    '7', '8', '9',
+    '4', '5', '6',
+    '1', '2', '3',
+    '0'
+  ]
+  const acciones = ['CE', '=']
+
+
+  // for, while, do while, forEach, map, filter, reduce
   return (
-    <div>App</div>
+    <main className="bg-blue-400 w-[340px] mx-auto mt-5 p-4 rounded-md shadow-md">
+      <h1 className="text-2xl font-bold text-center mb-4">
+        Calculadora con React
+      </h1>
+
+      <section>
+        <input
+          type="text"
+          readOnly
+          className="border"
+        />
+
+        <div>
+          {operaciones.map(operacion => {
+            return (
+              <button>{operacion}</button>
+            )
+          })}
+
+          {/* TODO: Recorrer el arreglo de numero y acciones y reenderizar su contenido en un boton. */}
+
+          {numeros}
+
+          {acciones}
+        </div>
+      </section>
+    </main>
   )
 }
 

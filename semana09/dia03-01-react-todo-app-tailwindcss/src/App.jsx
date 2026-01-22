754163dd0c35c698d8093cc207279659fb127333
Victor Villazón <victorzn23@gmail.com>
2026-01-21 21:53:25 -0500

    semana09, dia03, react todo app tailwind

--- semana09/dia03-01-react-todo-app-tailwindcss/src/App.jsx
+++ semana09/dia03-01-react-todo-app-tailwindcss/src/App.jsx
@@ -1,6 +1,32 @@
+import { useState } from "react"
+
 const App = () => {
+  const DEFAULT_TAREAS = [
+    {
+      id: '1',
+      titulo: 'Aprender Javascript',
+      completado: true
+    },
+    {
+      id: '2',
+      titulo: 'Aprender CSS',
+      completado: false
+    },
+    {
+      id: '3',
+      titulo: 'Aprender React.js + Tailwind CSS',
+      completado: false
+    },
+  ]
+
+  const [tareas, setTareas] = useState(DEFAULT_TAREAS)
+   
   return (
-    <div>App</div>
+    <div>
+      
+
+      <pre>{JSON.stringify(tareas, null, 2)}</pre>
+    </div>
   )
 }
 

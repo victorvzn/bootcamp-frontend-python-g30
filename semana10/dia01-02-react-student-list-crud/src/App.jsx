e1eba95e9b1b05a87ab791b706566d0a3bc16dd3
Victor Villazón <victorzn23@gmail.com>
2026-01-23 22:12:13 -0500

    semana09, dia03, react todo app tailwind

--- semana10/dia01-02-react-student-list-crud/src/App.jsx
+++ semana10/dia01-02-react-student-list-crud/src/App.jsx
@@ -1,49 +1,79 @@
 const App = () => {
   return (
     <main className="w-96 mx-auto border border-slate-400 rounded-lg mt-6 p-3">
-      <h1 className="text-2xl text-center text-slate-700 font-bold">Student list</h1>
+      <h1 className="text-2xl text-center text-slate-700 font-bold mb-4">Student CRUD</h1>
 
-      <form>
-        <label>
-          <span>Name</span>
+      <form
+        className="flex flex-col gap-4 bg-slate-100 p-3 rounded-lg border"
+      >
+        <label className="flex flex-col gap-2">
+          <span className="text-sm font-medium text-slate-900">Name</span>
           <input
+            className="bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg w-full px-3 py-2"
             type="text"
             name="name"
             placeholder="Ex. Victor Villazón"
           />
         </label>
 
-        <label>
-          <span>City</span>
+        <label className="flex flex-col gap-2">
+          <span className="text-sm font-medium text-slate-900">City</span>
           <input
+            className="bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg w-full px-3 py-2"
             type="text"
             name="city"
             placeholder="Ex. Chiclayo"
           />
         </label>
 
-        <div>
+        <div className="flex gap-4">
           <input
+            className="bg-blue-700 text-white hover:bg-blue-800 font-medium rounded-lg text-sm w-full px-5 py-3 text-center"
             type="submit"
             value="Save"
           />
           <input
+            className="bg-blue-700 text-white hover:bg-blue-800 font-medium rounded-lg text-sm w-full px-5 py-3 text-center"
             type="reset"
             value="Limpiar"
           />
         </div>
       </form>
 
-      <section>
-        <div>
+      <h2 className="text-center text-slate-700 font-bold my-4">Student List</h2>
+
+      <section className="mt-4 flex flex-col gap-2">
+        <div className="flex justify-between items-center gap-2 bg-slate-50 px-3 py-2 rounded-lg border">
           iMAGEN
-          <div>NAME</div>
-          <div>CITY</div>
-          <div>
+          <div className="text-left">NAME</div>
+          <div className="text-left">CITY</div>
+          <div className="flex gap-2">
             <button>✏</button>
             <button>❌</button>
           </div>
         </div>
+
+        <div className="flex justify-between items-center gap-2 bg-slate-50 px-3 py-2 rounded-lg border">
+          iMAGEN
+          <div className="text-left">NAME</div>
+          <div className="text-left">CITY</div>
+          <div className="flex gap-2">
+            <button>✏</button>
+            <button>❌</button>
+          </div>
+        </div>
+
+        <div className="flex justify-between items-center gap-2 bg-slate-50 px-3 py-2 rounded-lg border">
+          iMAGEN
+          <div className="text-left">NAME</div>
+          <div className="text-left">CITY</div>
+          <div className="flex gap-2">
+            <button>✏</button>
+            <button>❌</button>
+          </div>
+        </div>
+
+       
       </section>
     </main>
   )

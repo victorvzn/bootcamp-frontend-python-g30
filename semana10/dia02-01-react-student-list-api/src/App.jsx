3893b8adcae42b7f1b1c789006e12b17b152b0f6
Victor Villazón <victorzn23@gmail.com>
2026-01-26 22:43:25 -0500

    semana09, dia03, react todo app tailwind

--- semana10/dia02-01-react-student-list-api/src/App.jsx
+++ semana10/dia02-01-react-student-list-api/src/App.jsx
@@ -1,6 +1,30 @@
+// useEffect, no ayuda a controlar el ciclo de vida un componente
+// CREACIÓN, ACTUALIZACIÓN Y ELIMINACIÓN DEL COMPONENTE
+
+import { useState } from "react"
+import { useEffect } from "react"
+
 const App = () => {
+  const [contador, setContador] = useState(0)
+  const [students, setStudents] = useState([])
+
+  useEffect(() => {
+    console.log('CICLO DE VIDA: CREACIÓN / ACTUALIZACIÓN')
+  }, [contador]) // Se ejecuta el useEffect al cargar el componente la primera vez
+
+  useEffect(() => {
+    console.log('LA PRIMERA VEZ')
+    fetch('https://697828f05b9c0aed1e882a5d.mockapi.io/v1/students')
+      .then(response => response.json())
+      .then((data) => setStudents(data))
+  }, []) // Este useEffect se ejecuta la primera vez
+
   return (
-    <div>App</div>
+    <div>
+      
+      <button onClick={() => setContador(contador + 1)}>+1</button>
+
+    </div>
   )
 }
 

5e7d2460d1b88df907e67eb6798043171424a144
Victor Villazón <victorzn23@gmail.com>
2026-01-30 19:29:57 -0500

    supabase completo

--- semana10/dia03-01-react-crud-movie-app-supabase/src/App.jsx
+++ semana10/dia03-01-react-crud-movie-app-supabase/src/App.jsx
@@ -1,19 +1,33 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { supabase } from "./lib/supabase"
 
 const App = () => {
+  const [movies, setMovies] = useState([])
+
   useEffect(() => {
     const fetchMovies = async () => {
       const response = await supabase.from('movies').select('*')
 
-      console.log(response)
+      return response.data
     }  
 
     fetchMovies()
+      .then(data => setMovies(data))
   }, [])
 
   return (
-    <div>App</div>
+    <div>
+      
+      <h1 className="text-2xl">Movies</h1>
+
+      <ul className="list-disc">
+        {movies.map(movie => {
+          return (
+            <li key={movie.id}>{movie.name}</li>
+          )
+        })}
+      </ul>
+    </div>
   )
 }
 

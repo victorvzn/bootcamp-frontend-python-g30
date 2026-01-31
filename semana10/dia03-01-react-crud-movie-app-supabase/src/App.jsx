e267d38633744e24aa20e3124b1470cc88917101
Victor Villazón <victorzn23@gmail.com>
2026-01-30 19:39:42 -0500

    supabase completo

--- semana10/dia03-01-react-crud-movie-app-supabase/src/App.jsx
+++ semana10/dia03-01-react-crud-movie-app-supabase/src/App.jsx
@@ -17,16 +17,24 @@ const App = () => {
 
   return (
     <div>
-      
-      <h1 className="text-2xl">Movies</h1>
+      <section>
+        <h1 className="text-2xl">Movies</h1>
 
-      <ul className="list-disc list-inside pl-4">
-        {movies.map(movie => {
-          return (
-            <li key={movie.id}>{movie.name}</li>
-          )
-        })}
-      </ul>
+        <form>
+          <input type="text" name="name" className="border" placeholder="Nombre de la película" />
+          <input type="submit" value="Guardar" className="bg-orange-300" />
+        </form>
+
+        <h1 className="text-2xl">Lista de películas</h1>
+
+        <ul className="list-disc list-inside pl-4">
+          {movies.map(movie => {
+            return (
+              <li key={movie.id}>{movie.name}</li>
+            )
+          })}
+        </ul>
+      </section>
     </div>
   )
 }

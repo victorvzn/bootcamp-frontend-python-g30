f1bde67ed3817c668e62f8572bc3a76539779562
Victor Villazón <victorzn23@gmail.com>
2026-02-02 22:25:34 -0500

    semana11, dia02, react router declarative mode tailwind

--- semana11/dia02-01-react-router-declarative-mode-tailwind/src/pages/HomePage.jsx
+++ semana11/dia02-01-react-router-declarative-mode-tailwind/src/pages/HomePage.jsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react"
 
 import { fetchCharacters } from "../services/characters"
 
+import { Link } from 'react-router'
+
+// TODO: Crear la página CharacterPage y asociar los datos del personaje con el id que se visita. Titulo, boton regresar al home, imagen, name, species, status
+
 const HomePage = () => {
   // TODO: Listar los personajes(characters) de la api de Rick and Morty y deben mostrar los campos: image, id, name, species, location
   const [characters, setCharacters] = useState([])
@@ -18,16 +22,17 @@ const HomePage = () => {
       <section className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-8">
         {characters.map(character => {
           return (
-            <article
+            <Link
               key={character.id}
               className="bg-yellow-300 rounded-lg p-4 flex flex-col justify-evenly items-center gap-1"
+              href={`/characters/${character.id}`}
             >
               <img src={character.image} width={100} />
               <div className="font-bold text-xl">#{character.id}</div>
               <div className="font-bold text-3xl">{character.name}</div>
               <div className="font-bold bg-orange-300 text-slate-800 p-2 rounded-lg uppercase">{character.species}</div>
               <div className="font-bold">{character.location.name}</div>
-            </article>
+            </Link>
           )
         })}
       </section>

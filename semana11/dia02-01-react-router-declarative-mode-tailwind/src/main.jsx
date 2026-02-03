bf475c58dd16c64dfa8eb7965d6a6d60e7f4b6e6
Victor Villazón <victorzn23@gmail.com>
2026-02-02 21:03:32 -0500

    semana11, dia02, react router declarative mode tailwind

--- semana11/dia02-01-react-router-declarative-mode-tailwind/src/main.jsx
+++ semana11/dia02-01-react-router-declarative-mode-tailwind/src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router'
+import HomePage from './pages/HomePage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<h1>React Router - HOME PAGE</h1>} />
-      <Route path='/login' element={<h1>React Router - LOGIN PAGE</h1>} />
+      <Route path='/' element={<HomePage />} />
+      <Route path='/login' element={<LoginPage />} />
       <Route path='/register' element={<h1>React Router - REGISTER PAGE</h1>} />
     </Routes>
   </BrowserRouter>,

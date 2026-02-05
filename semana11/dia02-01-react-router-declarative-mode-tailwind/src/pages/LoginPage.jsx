26bf5d54ab5b228c904f9f2a04459f6e802e6ccb
Victor Villazón <victorzn23@gmail.com>
2026-02-04 20:57:23 -0500

    semana11, dia03, react router declarative mode tailwind

--- semana11/dia02-01-react-router-declarative-mode-tailwind/src/pages/LoginPage.jsx
+++ semana11/dia02-01-react-router-declarative-mode-tailwind/src/pages/LoginPage.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react"
+
 const LoginPage = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <>
       <h2>Login son supabase</h2>
@@ -6,17 +11,26 @@ const LoginPage = () => {
       <form>
         <label>
           Email
-          <input type="email" name="email"  />
+          <input
+            type="email"
+            name="email"
+            onChange={(event) => setEmail(event.target.value)}
+            value={email}
+          />
         </label>
 
         <label>
           Password
-          <input type="password" name="password"  />
+          <input
+            type="password"
+            name="password"
+            onChange={(event) => setPassword(event.target.value)}
+            value={password}
+          />
         </label>
 
         <input type="submit" value="Login" />
 
-        
       </form>
     </>
   )

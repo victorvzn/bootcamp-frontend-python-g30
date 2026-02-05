fbeae798f08e6c74fd48898fc5532ab325272a7a
Victor Villazón <victorzn23@gmail.com>
2026-02-04 21:49:59 -0500

    semana11, dia03, react router declarative mode tailwind

--- semana11/dia02-01-react-router-declarative-mode-tailwind/src/hooks/auth.js
+++ semana11/dia02-01-react-router-declarative-mode-tailwind/src/hooks/auth.js
@@ -1,17 +1,37 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { supabase } from "../lib/supabase"
 
 export function useAuth() {
   const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const getInitialSession = async () => {
+      const { data, error } = await supabase.auth.getSession()
+
+      setUser(data.session)
+    }
+
+    getInitialSession()
+  }, [])
+
+  useEffect(() => {
+    const { data: { subscription } } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setUser(session)
+      }
+    )
+
+    return () => subscription.unsubscribe()
+  }, [])
 
   const login = async (email, password) => {
     const { data, error } = await supabase.auth.signInWithPassword({ email, password })
 
     if (error) throw error
 
-    console.log(data, data.user)
+    console.log(data, data.session)
 
-    setUser(data?.user ?? null)
+    setUser(data?.session ?? null)
   }
 
   return {

7ec383261eabcaf4f7482eeabe304a126a13cdd7
Victor Villazón <victorzn23@gmail.com>
2026-02-04 19:45:25 -0500

    semana11, dia03, react router declarative mode tailwind

--- semana11/dia02-01-react-router-declarative-mode-tailwind/src/layouts/LayoutBase.jsx
+++ semana11/dia02-01-react-router-declarative-mode-tailwind/src/layouts/LayoutBase.jsx
@@ -1,11 +1,23 @@
-import { Outlet } from "react-router"
+import { Link, Outlet } from "react-router"
 
 const LayoutBase = () => {
   return (
     <>
-      <h1>LAYOUT BASE</h1>
+      <header className="bg-yellow-300 p-4">
+        <div className="container mx-auto flex justify-between">
+          <h1 className="font-bold">React router - Auth</h1>
 
-      <Outlet />
+          <nav className="flex gap-4">
+            <Link to='/login'>Login</Link>
+            <Link to='/register'>Register</Link>
+            <Link to='/'>Home</Link>
+          </nav>
+        </div>
+      </header>
+
+      <main className="container mx-auto m-6">
+        <Outlet />
+      </main>
     </>
   )
 }

a508dd8986a5f75269f029622ee0ca7b461fba18
Victor Villazón <victorzn23@gmail.com>
2026-02-06 22:53:47 -0500

    semana11, dia03, react router declarative mode tailwind

--- semana12/dia01-01-react-redux-counter-shopping-cart-app/src/components/ShoppingCart.jsx
+++ semana12/dia01-01-react-redux-counter-shopping-cart-app/src/components/ShoppingCart.jsx
@@ -1,6 +1,15 @@
+import { useSelector } from "react-redux"
+
 const ShoppingCart = () => {
+  const cart = useSelector(state => state.cart)
+
   return (
-    <div>ShoppingCart</div>
+    <>
+      <div>ShoppingCart</div>
+
+      <pre>{JSON.stringify(cart, null, 2)}</pre>
+
+    </>
   )
 }
 
--- semana12/dia01-01-react-redux-counter-shopping-cart-app/src/store/cart.js
+++ semana12/dia01-01-react-redux-counter-shopping-cart-app/src/store/cart.js
@@ -3,15 +3,23 @@ import { createSlice } from "@reduxjs/toolkit"
 export const CartSlice = createSlice({
   name: 'Cart',
   initialState: [
-    { id: 1, title: 'Product 1' },
-    { id: 2, title: 'Product 2' },
-    { id: 3, title: 'Product 3' },
+    // { id: 1, title: 'Product 1' },
+    // { id: 2, title: 'Product 2' },
+    // { id: 3, title: 'Product 3' },
   ],
   reducers: {
     addToCart: (state, action) => {
       // Aquí va la lógica para añadir un producto al carrito de compras
       // En action.payload está la información del producto(objeto)
       console.log('addToCart', action.payload)
+
+      const newProduct = action.payload
+
+      // Cuando el producto es nuevo en el carrito lo agregamos al estado directamente
+      return [
+        ...state,
+        newProduct
+      ]
     }
   }
 })

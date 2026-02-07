ae6946696f4ddc086cd07b02de77622206e8a884
Victor Villazón <victorzn23@gmail.com>
2026-02-06 23:04:54 -0500

    semana11, dia03, react router declarative mode tailwind

--- semana12/dia01-01-react-redux-counter-shopping-cart-app/src/components/ShoppingCart.jsx
+++ semana12/dia01-01-react-redux-counter-shopping-cart-app/src/components/ShoppingCart.jsx
@@ -1,15 +1,43 @@
 import { useSelector } from "react-redux"
 
+// TODO: Renderizar los productos añadidos al carrito de compras según la maquetación de abajo
+
 const ShoppingCart = () => {
   const cart = useSelector(state => state.cart)
 
   return (
-    <>
-      <div>ShoppingCart</div>
+    <section className="w-56 p-2">
+      <h3 className="text-2xl mb-2 text-center">ShoppingCart</h3>
+
+      <div className="mb-2">
+        <button
+          className="bg-red-400 p-2 min-w-36 rounded-lg cursor-pointer text-white font-bold hover:bg-red-500 duration-300 w-full"
+        >
+          Limpiar carrito
+        </button>
+      </div>
+
+      <ul className="flex flex-col gap-4">
+        <li
+          className="flex flex-col gap-2 font-bold bg-slate-200 p-2 rounded-lg shadow"
+        >
+          <span>title</span>
+          <span>S/ 0.00 (Qty: 1)</span>
+          <button
+            className="bg-red-400 p-2 rounded-lg cursor-pointer text-white font-bold hover:bg-red-500 duration-300"
+          >
+            ❌
+          </button>
+        </li>
+      </ul>
+
+      <div className="mt-4 pt-4 font-bold bg-amber-100 px-4 py-2 flex justify-between rounded-lg">
+        <strong>TOTAL:</strong> <span>S/ 0.00</span>
+      </div>
 
       <pre>{JSON.stringify(cart, null, 2)}</pre>
 
-    </>
+    </section>
   )
 }
 

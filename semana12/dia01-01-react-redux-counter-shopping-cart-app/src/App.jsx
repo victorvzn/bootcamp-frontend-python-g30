eab3429187e2bbc779437c00f97b70f5e1c384d5
Victor Villazón <victorzn23@gmail.com>
2026-02-06 21:01:16 -0500

    semana11, dia03, react router declarative mode tailwind

--- semana12/dia01-01-react-redux-counter-shopping-cart-app/src/App.jsx
+++ semana12/dia01-01-react-redux-counter-shopping-cart-app/src/App.jsx
@@ -1,8 +1,17 @@
 import Counter from "./components/Counter"
+import ProductList from "./components/ProductList"
 
 const App = () => {
   return (
     <main className="flex flex-col gap-8 p-4">
+      <section>
+
+        <ProductList />
+
+        {/* <ShoppingCart /> */}
+
+      </section>
+
       <Counter />
 
       <Counter />
---
+++ semana12/dia01-01-react-redux-counter-shopping-cart-app/src/components/ProductList.jsx
@@ -0,0 +1,9 @@
+const ProductList = () => {
+  // TODO: Listar los productos de la siguiente api "https://dummyjson.com/products" guardenlo en un useState. No usar Redux por el momento.
+
+  return (
+    <div>ProductList</div>
+  )
+}
+
+export default ProductList

1982d588ce628b034b86ef4ce4859955e13a1ac9
Victor Villazón <victorzn23@gmail.com>
2026-02-09 20:55:34 -0500

    semana12, dia02, react router declarative mode tailwind

--- semana12/dia01-01-react-redux-counter-shopping-cart-app/src/components/ShoppingCart.jsx
+++ semana12/dia01-01-react-redux-counter-shopping-cart-app/src/components/ShoppingCart.jsx
@@ -11,6 +11,29 @@ const ShoppingCart = () => {
 
   const dispatch = useDispatch()
 
+  const total = cart.reduce((acc, product) => {
+    const quantity = product.quantity
+    const price = product.price
+    const subtotal = quantity * price
+
+    return acc + subtotal
+  }, 0)
+
+  const isCartEmpty = cart.length === 0
+
+  if (isCartEmpty) {
+    return (
+      <section className="w-56 p-2">
+        <h3 className="text-2xl mb-2 text-center">Shopping cart</h3>
+
+        <div className="mb-2 bg-slate-300 h-32 rounded-lg p-4 flex flex-col items-center gap-4">
+          <span className="text-3xl">🛒</span>
+          <p className="text-lg text-center text-slate-500">Shopping cart empty</p>
+        </div>
+      </section>
+    )
+  }
+
   return (
     <section className="w-56 p-2">
       <h3 className="text-2xl mb-2 text-center">ShoppingCart</h3>
@@ -32,7 +55,7 @@ const ShoppingCart = () => {
               className="flex flex-col gap-2 font-bold bg-slate-200 p-2 rounded-lg shadow"
             >
               <span>{product.title}</span>
-              <span>S/ 0.00 (Qty: {product.quantity})</span>
+              <span>S/{product.price} (Qty: {product.quantity})</span>
               <button
                 className="bg-red-400 p-2 rounded-lg cursor-pointer text-white font-bold hover:bg-red-500 duration-300"
                 onClick={() => dispatch(removeFromCart(product.id))}
@@ -45,7 +68,7 @@ const ShoppingCart = () => {
       </ul>
 
       <div className="mt-4 pt-4 font-bold bg-amber-100 px-4 py-2 flex justify-between rounded-lg">
-        <strong>TOTAL:</strong> <span>S/ 0.00</span>
+        <strong>TOTAL:</strong> <span>S/{total}</span>
       </div>
 
       <pre>{JSON.stringify(cart, null, 2)}</pre>
